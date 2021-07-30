008120aa71661811cf9a84b9bac54478b032992e
Jonny Burger <jonathanburger11@gmail.com>
2021-07-30 14:21:59 +0200

    validate env

--- src/TextToSpeech/index.ts
+++ src/TextToSpeech/index.ts
@@ -1,9 +1,9 @@
+import {GetObjectCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import md5 from 'md5';
 import {
 	SpeechConfig,
 	SpeechSynthesizer,
 } from 'microsoft-cognitiveservices-speech-sdk';
-import {GetObjectCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
-import md5 from 'md5';
 
 const voices = {
 	ptBRWoman: 'pt-BR-FranciscaNeural',
@@ -11,6 +11,37 @@ const voices = {
 	enUSWoman1: 'en-US-JennyNeural',
 	enUSWoman2: 'en-US-AriaNeural',
 } as {[key: string]: string};
+
+if (!process.env.AZURE_TTS_KEY) {
+	throw new Error(
+		'AZURE_TTS_KEY environment variable is missing. Read the docs first and complete the setup.'
+	);
+}
+if (!process.env.AZURE_TTS_REGION) {
+	throw new Error(
+		'AZURE_TTS_REGION environment variable is missing. Read the docs first and complete the setup.'
+	);
+}
+if (!process.env.AWS_S3_BUCKET_NAME) {
+	throw new Error(
+		'AWS_S3_BUCKET_NAME environment variable is missing. Read the docs first and complete the setup.'
+	);
+}
+if (!process.env.AWS_S3_REGION) {
+	throw new Error(
+		'AWS_S3_REGION environment variable is missing. Read the docs first and complete the setup.'
+	);
+}
+if (!process.env.AWS_ACCESS_KEY_ID) {
+	throw new Error(
+		'AWS_ACCESS_KEY_ID environment variable is missing. Read the docs first and complete the setup.'
+	);
+}
+if (!process.env.AWS_SECRET_ACCESS_KEY) {
+	throw new Error(
+		'AWS_SECRET_ACCESS_KEY environment variable is missing. Read the docs first and complete the setup.'
+	);
+}
 
 export const textToSpeech = async (
 	text: string,
