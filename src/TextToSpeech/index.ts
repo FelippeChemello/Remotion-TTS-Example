7340fa1cb849bfd9de6a4d7aa1b4a7223b3f169b
Jonny Burger <jonathanburger11@gmail.com>
2021-07-30 15:20:51 +0200

    validation in video.tsx

--- src/TextToSpeech/index.ts
+++ src/TextToSpeech/index.ts
@@ -12,37 +12,6 @@ const voices = {
 	enUSWoman2: 'en-US-AriaNeural',
 } as {[key: string]: string};
 
-if (!process.env.AZURE_TTS_KEY) {
-	throw new Error(
-		'AZURE_TTS_KEY environment variable is missing. Read the docs first and complete the setup.'
-	);
-}
-if (!process.env.AZURE_TTS_REGION) {
-	throw new Error(
-		'AZURE_TTS_REGION environment variable is missing. Read the docs first and complete the setup.'
-	);
-}
-if (!process.env.AWS_S3_BUCKET_NAME) {
-	throw new Error(
-		'AWS_S3_BUCKET_NAME environment variable is missing. Read the docs first and complete the setup.'
-	);
-}
-if (!process.env.AWS_S3_REGION) {
-	throw new Error(
-		'AWS_S3_REGION environment variable is missing. Read the docs first and complete the setup.'
-	);
-}
-if (!process.env.AWS_ACCESS_KEY_ID) {
-	throw new Error(
-		'AWS_ACCESS_KEY_ID environment variable is missing. Read the docs first and complete the setup.'
-	);
-}
-if (!process.env.AWS_SECRET_ACCESS_KEY) {
-	throw new Error(
-		'AWS_SECRET_ACCESS_KEY environment variable is missing. Read the docs first and complete the setup.'
-	);
-}
-
 export const textToSpeech = async (
 	text: string,
 	voice: string
